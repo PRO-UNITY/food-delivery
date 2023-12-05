dee1362fad6490719a7d273a6635b3abaf6b56d1
Ibrokhim1006 <Ibroxim.2001@mail.ru>
2023-12-05 05:47:00 -0500

    Foods

--- back-end/kitchen/urls.py
+++ back-end/kitchen/urls.py
@@ -11,10 +11,10 @@ from kitchen.views import (
 
 urlpatterns = [
     path("kitchen_create", KitchenCreateViews.as_view()),
-    path("kitchen_crud<int:pk>", KitchenCrudViews.as_view()),
+    path("kitchen_crud/<int:pk>", KitchenCrudViews.as_view()),
     path('all_kitchen', AllKitchenViews.as_view()),
     path("kitchen_foods", KitchenFoodsViews.as_view()),
-    path("kitchen_foods_crud<int:pk>", KitchenFoodsCrudViews.as_view()),
+    path("kitchen_foods_crud/<int:pk>", KitchenFoodsCrudViews.as_view()),
     path("all_kitchen_foods", AllKitchenFood.as_view()),
-    path("deteile_kitchen_food<int:pk>", DeteileKitchenFood.as_view()),
+    path("deteile_kitchen_food/<int:pk>", DeteileKitchenFood.as_view()),
 ]
