5244c8ecc9578afe7e9432c44c22f4f4c1899b52
Ibrokhim1006 <Ibroxim.2001@mail.ru>
2023-12-12 02:34:44 -0500

    Success

---
+++ back-end/main.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# PostgreSQL Database Credentials
+DB_USER="postgres"
+DB_PASSWORD="1"
+DB_NAME="resipe"
+
+# Set the PGPASSWORD environment variable for this session
+export PGPASSWORD=$DB_PASSWORD
+
+# Drop the existing database
+dropdb -U $DB_USER -h localhost $DB_NAME
+
+# Create a new database
+createdb -U $DB_USER -h localhost $DB_NAME
+
+# Restore from backup
+psql -U $DB_USER -h localhost -d $DB_NAME < ~/Desktop/main.sql
+
+# Unset the PGPASSWORD environment variable
+unset PGPASSWORD
