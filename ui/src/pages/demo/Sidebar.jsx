7fc4f9204b9818e232b265740b9c21680cb1e625
naimovdavlatshoh <davlatshoh.fullstack@gmail.com>
2023-12-19 17:09:27 +0500

    changed urls

--- ui/src/pages/demo/Sidebar.jsx
+++ ui/src/pages/demo/Sidebar.jsx
@@ -3,31 +3,6 @@ import { NavLink, Link } from "react-router-dom";
 import { useEffect, useState } from 'react';
 import { getDataWithToken, BASE_URL } from '../../functions/function';
 
-const Sidenavs = [
-    {
-      id: "foods",
-      name: "Favourite",
-      path: "/favourite",
-      active: "/favourite",
-      icon: "fa-regular fa-heart",
-    },
-    {
-      id: "deliveryman",
-      name: "Order History",
-      path: "/order-history",
-      active: "/order-history",
-      icon: "fa-solid fa-clock-rotate-left",
-
-    },
-    {
-      id: "orders",
-      name: "Settings",
-      path: "/settings",
-      active: "/settings",
-      icon: "fa-solid fa-gears",
-    }
-];
-
 const DemoSidebar = ({ showSidebar }) => {
 
     const [user, setUser] = useState(null)
@@ -70,12 +45,12 @@ const DemoSidebar = ({ showSidebar }) => {
                 </NavLink>
               </li>  
               <li className="nav-item">
-                <NavLink to={localStorage.getItem('token')?"/favourite":"/login"} className="nav-link text-start px-2" isActive={() => checkActive("/order-history")}>
+                <NavLink to={localStorage.getItem('token')?"/order-history":"/login"} className="nav-link text-start px-2" isActive={() => checkActive("/order-history")}>
                     <p className="text-start py-3"><i className="fa-solid fa-clock-rotate-left mx-3"></i> Order History</p>
                 </NavLink>
               </li>  
               <li className="nav-item">
-                <NavLink to={localStorage.getItem('token')?"/favourite":"/login"} className="nav-link text-start px-2" isActive={() => checkActive("/settings")}>
+                <NavLink to={localStorage.getItem('token')?"/settings":"/login"} className="nav-link text-start px-2" isActive={() => checkActive("/settings")}>
                     <p className="text-start py-3"><i className="fa-solid fa-gears mx-3"></i> Settings</p>
                 </NavLink>
               </li>  
