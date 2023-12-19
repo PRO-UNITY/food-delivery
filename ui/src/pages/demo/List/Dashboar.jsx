c12ae1e9e2ad51aa48b387b8930a1a2b4f05e6ae
naimovdavlatshoh <davlatshoh.fullstack@gmail.com>
2023-12-19 14:51:57 +0500

    added responsive

--- ui/src/pages/demo/List/Dashboar.jsx
+++ ui/src/pages/demo/List/Dashboar.jsx
@@ -37,9 +37,11 @@ const Dashboard = () => {
         })
     },[])
 
-    if (loading) {
-        return (
-            <div className="container d-flex justify-content-center align-items-center py-5">
+    return ( 
+        <DemoLayout>
+            {
+                loading?
+                <div className="container body-main d-flex justify-content-center align-items-center py-5">
                 <Button variant="warning" disabled>
                     <Spinner
                     as="span"
@@ -50,83 +52,81 @@ const Dashboard = () => {
                     />
                     Loading...
                 </Button>
-            </div>
-        )
-    }
-    return ( 
-        <DemoLayout>
+            </div>:
             <div className="w-100 body-main  p-5">
-                {/* <div className="vaucher d-flex flex-column justify-content-center align-items-start p-4 mb-4">
-                    <h3 className="text-white">Det Discount Vaucher <br /> Up To 20%</h3>
-                    <p className="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> Neque reiciendis sit doloremque aliquam </p>
-                </div> */}
-                <div className="d-flex justify-content-between align-items-center mb-3">
-                <h3>Category</h3>
-                <button className="orange text-center mb-1 btn-none">View All <i className="fa-solid fa-angle-right"></i></button>
-                </div>
-                <div className="categories w-100 mb-3">
-                    {
-                        category.map((item,index)=>
-                            <Link to={'/'} className="text-dark" style={{textDecoration:"none"}}>
-                            <div className="category-item bg-white">
-                                <img style={{width:"50px",height:"50px", borderRadius:"10px"}} src={`${item?.kitchen.logo ? BASE_URL+item.kitchen.logo:"https://www.freeiconspng.com/uploads/food-icon-7.png"}`} />
-                                <p className="name-category">{item.name}</p>
-                            </div>
-                            </Link>
-                        )
-                    }
-                </div>
-                <div className="d-flex justify-content-between align-items-center mb-3">
-                <h3>Restaurants</h3>
-                <button className="orange text-center mb-1 btn-none">View All <i className="fa-solid fa-angle-right"></i></button>
-                </div>
-                <div className="categories w-100 mb-3">
-                    {
-                        kitchen.map((item, index)=>
-                        <Link key={index} to={'/'} className="text-dark" style={{textDecoration:"none"}}>
+            {/* <div className="vaucher d-flex flex-column justify-content-center align-items-start p-4 mb-4">
+                <h3 className="text-white">Det Discount Vaucher <br /> Up To 20%</h3>
+                <p className="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> Neque reiciendis sit doloremque aliquam </p>
+            </div> */}
+            <div className="d-flex justify-content-between align-items-center mb-3">
+            <h3>Category</h3>
+            <button className="orange text-center mb-1 btn-none">View All <i className="fa-solid fa-angle-right"></i></button>
+            </div>
+            <div className="categories w-100 mb-3">
+                {
+                    category.map((item,index)=>
+                        <Link to={'/'} className="text-dark" style={{textDecoration:"none"}}>
                         <div className="category-item bg-white">
-                            <img style={{width:"50px",height:"50px", borderRadius:"10px"}} src={`${item?.logo? BASE_URL+item.logo:"https://www.freeiconspng.com/uploads/food-icon-7.png"}`} />
+                            <img style={{width:"50px",height:"50px", borderRadius:"10px"}} src={`${item?.kitchen.logo ? BASE_URL+item.kitchen.logo:"https://www.freeiconspng.com/uploads/food-icon-7.png"}`} />
                             <p className="name-category">{item.name}</p>
                         </div>
                         </Link>
-                        )
-                    }
-                </div>
-                <div className="d-flex justify-content-between align-items-center mb-3">
-                <h3>Popular Foods</h3>
-                <button className="orange text-center mb-1 btn-none">View All <i className="fa-solid fa-angle-right"></i></button>
-                </div>
-                <div className="foods">
-                    {
-                        food.map((item, index)=>
-                        <Link key={index} className="food-item bg-white  text-dark" style={{textDecoration:"none"}}>
-                        
-                        <div className="w-100 d-flex justify-content-center">
-                        <img style={{width:"160px", height:"160px", objectFit:"contain", borderRadius:"20px"}} src={`${item?.food_img? BASE_URL+item.food_img:"https://www.freeiconspng.com/uploads/food-icon-7.png"}`} />
-                        </div>
-                        {/* <div className="mb-2">
-                        <i className="fa-solid fa-star orange"></i>
-                        <i className="fa-solid fa-star orange"></i>
-                        <i className="fa-solid fa-star orange"></i>
-                        <i className="fa-solid fa-star orange"></i>
-                        <i className="fa-solid fa-star orange"></i>
-                        </div> */}
-                        <div className="d-flex justify-content-between w-100 align-items-center">
-                            <div>
-                            <p className="p-0 m-0">{item.name}</p>
-                            <p style={{fontWeight:800}}><span className="orange">$</span>{item.price}</p>
-                            </div>
-                            <button className="btn-add bg-orange"><i className="fa-solid fa-plus"></i></button>
-                        </div>
-                        {/* <div className="sale">
-                            <div className="d-flex justify-content-center align-items-center px-2 text-white sale-percent">15% Off</div>
-                            <button style={{color:"rgb(247, 69, 69)"}} className="btn-favourite"><i className="fa-solid fa-heart"></i></button>
-                        </div> */}
-                        </Link>
-                        )
-                    }   
-                </div>
+                    )
+                }
             </div>
+            <div className="d-flex justify-content-between align-items-center mb-3">
+            <h3>Restaurants</h3>
+            <button className="orange text-center mb-1 btn-none">View All <i className="fa-solid fa-angle-right"></i></button>
+            </div>
+            <div className="categories w-100 mb-3">
+                {
+                    kitchen.map((item, index)=>
+                    <Link key={index} to={'/'} className="text-dark" style={{textDecoration:"none"}}>
+                    <div className="category-item bg-white">
+                        <img style={{width:"50px",height:"50px", borderRadius:"10px"}} src={`${item?.logo? BASE_URL+item.logo:"https://www.freeiconspng.com/uploads/food-icon-7.png"}`} />
+                        <p className="name-category">{item.name}</p>
+                    </div>
+                    </Link>
+                    )
+                }
+            </div>
+            <div className="d-flex justify-content-between align-items-center mb-3">
+            <h3>Popular Foods</h3>
+            <button className="orange text-center mb-1 btn-none">View All <i className="fa-solid fa-angle-right"></i></button>
+            </div>
+            <div className="foods">
+                {
+                    food.map((item, index)=>
+                    <Link key={index} className="food-item bg-white  text-dark" style={{textDecoration:"none"}}>
+                    
+                    <div className="w-100 d-flex justify-content-center">
+                    <img style={{width:"160px", height:"160px", objectFit:"contain", borderRadius:"20px"}} src={`${item?.food_img? BASE_URL+item.food_img:"https://www.freeiconspng.com/uploads/food-icon-7.png"}`} />
+                    </div>
+                    {/* <div className="mb-2">
+                    <i className="fa-solid fa-star orange"></i>
+                    <i className="fa-solid fa-star orange"></i>
+                    <i className="fa-solid fa-star orange"></i>
+                    <i className="fa-solid fa-star orange"></i>
+                    <i className="fa-solid fa-star orange"></i>
+                    </div> */}
+                    <div className="d-flex justify-content-between w-100 align-items-center">
+                        <div>
+                        <p className="p-0 m-0">{item.name}</p>
+                        <p style={{fontWeight:800}}><span className="orange">$</span>{item.price}</p>
+                        </div>
+                        <button className="btn-add bg-orange"><i className="fa-solid fa-plus"></i></button>
+                    </div>
+                    {/* <div className="sale">
+                        <div className="d-flex justify-content-center align-items-center px-2 text-white sale-percent">15% Off</div>
+                        <button style={{color:"rgb(247, 69, 69)"}} className="btn-favourite"><i className="fa-solid fa-heart"></i></button>
+                    </div> */}
+                    </Link>
+                    )
+                }   
+            </div>
+        </div>
+            }
+            
         </DemoLayout>
     )
 }
