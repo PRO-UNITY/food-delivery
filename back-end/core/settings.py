995a6cdfaa90ce97999475bef951d54f957bc78b
Ibrokhim1006 <Ibroxim.2001@mail.ru>
2023-12-22 01:59:46 -0500

    success

--- back-end/core/settings.py
+++ back-end/core/settings.py
@@ -148,11 +148,11 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
 
-MEDIA_URL = "/media/"
+MEDIA_URL = "food-delivery/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
