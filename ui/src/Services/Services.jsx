8f6b7803ea709831971cde6adc57ad5a4179c6ba
naimovdavlatshoh <davlatshoh.fullstack@gmail.com>
2024-01-23 09:17:21 +0500

    changed BASE_URL

--- ui/src/Services/Services.jsx
+++ ui/src/Services/Services.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// export const BASE_URL = "https://api.prounity.uz/food-delivery";
-export const BASE_URL = "http://192.168.0.114:8000/";
+export const BASE_URL = "https://api.prounity.uz/food-delivery";
+// export const BASE_URL = "http://192.168.0.114:8000/";
 
 export const customFetch = async (url, options = {}) => {
   const token = localStorage.getItem("token");
